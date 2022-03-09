e7b247d998062f8ad17bd61f0947209cbe2fdafe
Nikita <nikitakhapare53@gmail.com>
2022-03-09 07:35:08 +0530

    removing: function to sort entring by using first Name

--- CreateAddBook.java
+++ CreateAddBook.java
@@ -183,10 +183,6 @@ public int countPersonsByState(String state) {
 		}
 		return count;
 	}
-	public void sortByFirstName() {
-		List<AddressBook>  sortedList = (ArrayList<AddressBook>) contact.stream().sorted((type1, type2) -> (type1.getFirstName().compareTo(type2.getFirstName()))).collect(Collectors.toList());
-		for (AddressBook contact : sortedList) {
-			System.out.println("Name: " + contact.getFirstName()+ "  " + contact.getLastName());
-		}
+
 	}
 }
